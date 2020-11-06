4114fc6ceefb075283465352e1cd81aa2803f5ff
neroru-github <9276952+neroru-github@users.noreply.github.com>
2020-11-07 00:26:13 +0900

    .jsに修正 サイズ表示追加

---
+++ index.js
@@ -0,0 +1,9 @@
+const width = document.documentElement.clientWidth;
+document.querySelector( "#Width span" ).textContent = String( width );
+
+const height = document.documentElement.clientHeight;
+document.querySelector( "#Height span" ).textContent = String( height );
+
+const pixcelRatio = window.devicePixelRatio;
+document.querySelector( "#PixcelRatio span" ).textContent = String( pixcelRatio );
+
